ae491b248ae2d720a5536c8b8fd132fb0d6fc487
William Nguyen <nguyew2@uw.edu>
2018-11-07 12:32:53 -0800

    started cleaning

---
+++ term_clean.py
@@ -0,0 +1,13 @@
+#https://www.techbeamers.com/merge-multiple-csv-files/
+#https://codereview.stackexchange.com/questions/186483/using-pandas-to-merge-csv-files
+#https://franciscomorales.org/2017/03/10/how-to-python-combine-multiple-csv-files-into-one/ 
+#https://www.techbeamers.com/merge-multiple-csv-files/
+
+#Common Terms
+#matchID ()
+#teamID
+
+#playoff Stage has different IDs
+
+#team (not as good) and team 1 line  give team IDs
+
